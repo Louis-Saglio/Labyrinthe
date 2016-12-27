fb66b4795fc00a2af6126486f36bd813050193ee
Louis-Saglio <louis.saglio@sfr.fr>
2016-12-27 11:59:12 +0100

    ajout de lire compte amélioaration de creer_compte

--- sessions.py
+++ sessions.py
@@ -1,6 +1,7 @@
 # Créer un compte
 # enregistrer scores
 # consulter compte
+# chiffrer le mdp
 
 def creer_compte():
     from pickle import Pickler
@@ -8,6 +9,29 @@ def creer_compte():
     chdir('saved_game')
     user_name = input("Entrez un nom d'utilisateur")
     password = input("Choisissez un mot-de-passe")
-    with open(user_name, 'w') as fichier:
+    data = {
+        'password': password,
+        'scores' : ['rien']
+        }
+    with open(user_name, 'wb') as fichier:
         mon_pickler = Pickler(fichier)
-        mon_pickler.dump('password : ' + password + '/n')
+        mon_pickler.dump(password)
+    chdir('..')
+
+def lire_compte(user_name, password):
+    from pickle import Unpickler
+    from os import chdir
+    chdir('saved_game')
+    with open(user_name, 'rb') as fichier:
+        depickler = Unpickler(fichier)
+        data = depickler.load()
+        print(data)
+    chdir('..')
+    return data
+
+def test_sessions():
+    creer_compte()
+    a = lire_compte('louis', 'erty')
+    print(a)
+
+test_sessions()
