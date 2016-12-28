f9f54e4874aa92fa41a965feabc911b58de8ce87
Louis-Saglio <louis.saglio@sfr.fr>
2016-12-28 20:02:35 +0100

    Ajout d'un mode verbose pour creer_compte Fixation du mode verbose de vérifier_password Ajout d'une fonction connexion Mise à jour de le pdt

--- sessions.py
+++ sessions.py
@@ -1,7 +1,10 @@
 from module import *
 
-def creer_compte(user_name, password):
+def creer_compte(user_name='', password='', verbose=False):
     from pickle import Pickler
+    if verbose is True:
+        user_name = input("Choisissez un nom d'utilisateur")
+        password = input("Choisissez un mot de passe")
     password = chiffrage(password, password)
     data = {
         'password': password,
@@ -30,15 +33,26 @@ def enregistrer_score(user, password, newScore):
     else:
         print('mot de passe incorrect')
 
-def verifier_password(user_name, password, demander=False):
-    if demander is False:
-        data = lire_compte(user_name)
-        real_password = data['password']
-    else:
-        real_password = input("Mot-de-passe ?")
+def verifier_password(user_name, password='', demander=False):
+    if demander:
+        password = input("Mot-de-passe ?")
+    data = lire_compte(user_name)
+    real_password = data['password']
     if chiffrage(password, password) == real_password:
         return True
     else:
+        return False
+
+def connexion(mode='verbose', user_name='', password=''):
+    if mode == 'verbose':
+        user_name = input("Quel est votre nom d'utilisateur ?")
+        password = input("Mot-de-passe ?")
+    try:
+        assert verifier_password(user_name, password) is True
+        return user_name
+    except:
+        if mode == 'verbose':
+            print("Nom d'utilisateur ou mot de passe invalide")
         return False
 
 if __name__ == "__main__":
@@ -50,4 +64,6 @@ if __name__ == "__main__":
     enregistrer_score(user, password, 25)
     enregistrer_score(user, password, 34)
     assert verifier_password(user, password) is True
+    connexion('autre', user, password)
+    connexion('autre', gen_mot_rand(), gen_mot_rand())
     remove('saved_game/' + user)
