1c2c2d95287dbf91bbadee297c8145bfdb01e87e
Louis-Saglio <louis.saglio@sfr.fr>
2016-12-30 13:33:12 +0100

    Amélioration de enregistrer_score Ajout des fonctions creer_meilleurs_scores et ajouter_meilleurs_scores upload de la pdt

--- sessions.py
+++ sessions.py
@@ -23,7 +23,7 @@ def lire_compte(user_name):
         data = depickler.load()
     return data
 
-def enregistrer_score(user, password, newScore):
+def enregistrer_score(user, password='', newScore=0):
     from pickle import Pickler
     data = lire_compte(user)
     password = chiffrage(password, password)
@@ -57,6 +57,42 @@ def connexion(mode='verbose', user_name='', password=''):
             print("Nom d'utilisateur ou mot de passe invalide")
         return False
 
+def creer_meilleurs_scores(fichier='meilleurs_scores'):
+    from pickle import Pickler
+    data = {
+        'user_name': [],
+        'scores' : []
+        }
+    with open('saved_game/' + fichier, 'wb') as fichier:
+        mon_pickler = Pickler(fichier)
+        mon_pickler.dump(data)
+    return data
+
+def ajouter_meilleurs_scores(user, score):
+    from pickle import Pickler
+    data = lire_compte('meilleurs_scores')
+    lstScores = data['scores']
+    print(lstScores)
+    lstUsers = data['user_name']
+    print(lstUsers)
+    lstScores.append(score)
+    print(lstScores)
+    lstUsers.append(user)
+    print(lstUsers)
+    data = {
+        'user_name': [lstUsers],
+        'scores' : [lstScores]
+        }
+    print(data)
+    with open('saved_game/meilleurs_scores', 'wb') as fichier:
+            mon_pickler = Pickler(fichier)
+            mon_pickler.dump(data)
+
+def afficher_meilleurs_scores(fichier='meilleurs_scores'):
+    data = lire_compte(fichier)
+    for i in range(len(data['scores'])):
+        print(data['user_name'][i], ' : ', data['scores'][i])
+
 if __name__ == "__main__":
     # test de creer_compte, enregistrer_score, lire_compte, vérifier_password
     from os import remove
@@ -69,3 +105,8 @@ if __name__ == "__main__":
     connexion('autre', user, password)
     connexion('autre', gen_mot_rand(), gen_mot_rand())
     remove('saved_game/' + user)
+    creer_meilleurs_scores()
+    ajouter_meilleurs_scores(user, 15)
+    ajouter_meilleurs_scores(user, 15)
+    ajouter_meilleurs_scores(user, 15)
+    afficher_meilleurs_scores()
